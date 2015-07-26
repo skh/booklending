fb26190993fa5931a4691099a3dcd2d4178646b6
Sonja Krause-Harder <krauseha@gmail.com>
2015-07-26 20:41:58 +0200

    Add fakedata

---
+++ fakedata.py
@@ -0,0 +1,5 @@
+ # -*- coding: utf-8 -*-
+
+cities = [{'name': 'Nürnberg', 'country': 'Germany', 'image': 'nuernberg.png', 'id': 1},
+          {'name': 'San Francisco', 'country': 'USA', 'image': 'sanfrancisco.png', 'id': 2},
+          {'name': 'Prague', 'country': 'Czech Republic', 'image': 'prague.png', 'id': 3}]
